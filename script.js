8e782108e5085872a92994fc3fae5d9a045b9ce2
Guillermo <memolvpz@gmail.com>
2023-09-01 21:19:16 -0600

    staring conditions and first elements created, also .hidden class added to css

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 'use strict';
+
+// Selecting elements.
+const score = document.querySelector('#score--0');
+const score1 = document.getElementById('score--1');
+const dice = document.querySelector('.dice');
+
+// Staring conditions.
+score.textContent = 0;
+score1.textContent = 0;
+dice.classList.add('hidden');
